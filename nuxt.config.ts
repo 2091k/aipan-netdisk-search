61a14880c8fec587f9d965969064d49b7efd596c
2091k <140614254+2091k@users.noreply.github.com>
2024-08-17 12:01:58 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
     app: {
         // head
         head: {
-            title: '爱盼-网盘资源搜索',
+            title: '魏无羡-网盘资源搜索',
             meta: [
                 {
                     name: 'description',
